5ed26e81e39d995bd32cdbf11adad53694ab9f03
Sow1995 <leondeklerk12@gmail.com>
2020-02-15 23:34:23 +0100

    starting with adding up arrays

--- app.js
+++ app.js
@@ -11,13 +11,7 @@ const user = 'Leon de Klerk';
 const nonce = null;
 
 // Excecute
-axios.get(getUrl)
-.then(function (newBlock) {
-
-})
-.catch(function (error) {
-	console.log(error)
-});
+mod10sha('dit is 1011111111')
 
 
 // Functions
@@ -27,6 +21,17 @@ axios.get(getUrl)
 		//mine block
 	}
 
+	// Get next block
+	function getNextBlock() {
+		axios.get(getUrl)
+		.then(function (block) {
+			return block
+		})
+		.catch(function (error) {
+			console.log(error)
+		});
+	}
+	
 	// Post nonce to blockchain
 	function postNonce() {
 		axios.post(postUrl, {
@@ -44,4 +49,52 @@ axios.get(getUrl)
 	// Convert data to sha265
 	function to265(data){
 		return crypto.createHash('sha256').update(data).digest('hex');
+	}
+
+	function mod10sha(data){
+		data = data.replace(/\s+/g, '');
+
+		let numberArray = []
+		data.split('').forEach(function (n) {
+			if(!(isNaN(n))){
+				numberArray.push(n)
+			}else{
+				numberArray.push(n.charCodeAt(0));
+			}
+		});
+
+		let splitArray = []
+		for (let val of numberArray) {
+			val.toString().split('').forEach(function (n){
+				splitArray.push(parseInt(n))
+			})
+		}
+
+		
+		let counted = splitArray.length
+		let howMuch = 10 - (counted%10)
+		for (let i = 0; i < howMuch; i++) {
+			splitArray.push(i)
+		}
+
+		let multipleOfTenArray = []
+        while (splitArray.length) {
+			multipleOfTenArray.push(splitArray.splice(0, 10));
+		}
+
+		let lastArray = []
+		for (let i = 0; i < (multipleOfTenArray.length - 1); i++){
+			for (let o = 0; o < 10; o++){
+				lastArray.push(multipleOfTenArray[i][o] + multipleOfTenArray[(i+1)][o])
+			}
+		}
+
+		let lastString = lastArray.toString().replace(/,/g, '');
+		
+
+
+		console.log('Multiple of 10 array:')
+		console.log(multipleOfTenArray)
+		console.log(lastString)
+
 	}
