323998a5264e567f16f296004a6b758dd49d6019
Sow1995 <leondeklerk12@gmail.com>
2021-02-11 21:00:05 +0100

    testing

--- app.js
+++ app.js
@@ -51,7 +51,9 @@ function postNonce(n) {
 }
 
 function createLastBlockString(b){
-	return b.blockchain.hash + b.blockchain.data[0].from + b.blockchain.data[0].to + b.blockchain.data[0].amount + b.blockchain.data[0].timestamp + b.blockchain.timestamp + b.blockchain.nonce;
+	//return b.blockchain.hash + b.blockchain.data[0].from + b.blockchain.data[0].to + b.blockchain.data[0].amount + b.blockchain.data[0].timestamp + b.blockchain.timestamp + b.blockchain.nonce;
+	//testdata
+	return '000078454c038871fa4d67b0022a30baaf25eaa231f8991b108e2624f052f3f8CMGT Mining CorporationBob PIKAB11548689513858154874778871610312'
 }
 
 function createLastBlockHash(lastBlockString){
@@ -60,18 +62,21 @@ function createLastBlockHash(lastBlockString){
 }
 
 function createNewBlockString(h, b){
-	return h + b.transactions[0].from + b.transactions[0].to + b.transactions[0].amount + b.transactions[0].timestamp + b.timestamp;
+	//return h + b.transactions[0].from + b.transactions[0].to + b.transactions[0].amount + b.transactions[0].timestamp + b.timestamp;
+	//testdata
+	return h + 'CMGT Mining CorporationBas BOOTB115487477332611548748101396'
 }
 
 function findNonceWithNumbers(d){
 	for( let i = 1500; 1 == 1; i++){
-		nonce = 'https://blockchain.cmgt.dev?' + i + '&name=Leon%200955849';
-		//nonce = 'https://www.youtube.com/watch?v=kMlLz7stjwc?' + i + '????CHECK-OUT->>>>>>>>>https://BLOCKCHAIN.CMGT.DEV' ; 
+		let nonce = i;
+		//let nonce = 'https://blockchain.cmgt.dev?' + i + '&name=Leon%200955849';
+		//let nonce = 'https://www.youtube.com/watch?v=kMlLz7stjwc?' + i + '????CHECK-OUT->>>>>>>>>https://BLOCKCHAIN.CMGT.DEV' ; 
 		let w = to265(mod10sha(d + nonce));
 		if(isThisAZero(w, nonce)){
 			return nonce;
 		}
-	};
+	}
 }
 
 function to265(d){
@@ -104,7 +109,7 @@ const mod10sha = (data) => {
 		val.toString().split('').forEach(function (n){
 			splitArray.push(parseInt(n));
 		})
-	};
+	}
 
 	let howMuch = 10 - (splitArray.length%10);
 	for (let i = 0; i < howMuch; i++) {
